8c9f8f0bf2ddfd1f071b494b75b061efe68aad19
felipe rincon <luis.rincon@celuweb.com>
2020-10-10 14:46:30 -0500

    initial commit

---
+++ src/main/java/com/software/masajes/MasajesApplication.java
@@ -0,0 +1,13 @@
+package com.software.masajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasajesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/model/Tearapeuta.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Tearapeuta {
+	
+	
+	
+	private String cedula;
+	private String clave;
+	private String direccion;
+	private String email;
+	private String nombre;
+	private String profesion;
+	
+	
+	
+
+}
