a5ba458f085de6f4d9acda71f554469bf6afc9fb
damarin <damarin@uqvirtual.edu.co>
2020-10-13 23:14:37 -0500

    Solo falta el update del controlador de cliente, ya se hizo del de delete

--- src/main/java/com/software/masajes/controladores/ClienteController.java
+++ src/main/java/com/software/masajes/controladores/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -101,4 +102,13 @@ public class ClienteController {
 		}
 	}
 	
+	@DeleteMapping("/clientes/{id}")
+	public ResponseEntity<HttpStatus> deleteCliente(@PathVariable("id") long id) {
+		try {
+			clienteRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
