58772b428addf266196128afe6350b79b911d6e9
damarin <damarin@uqvirtual.edu.co>
2020-11-08 21:25:01 -0500

    Se creo el controlador de terapia. Falta la parte de crear terapias y de actualizar terapias.

--- src/main/java/com/software/masajes/controladores/TerapiaController.java
+++ src/main/java/com/software/masajes/controladores/TerapiaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -67,6 +68,16 @@ public class TerapiaController {
 			return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("/terapias/{id}")
+	public ResponseEntity<HttpStatus> deleteTerapia(@PathVariable("id") long id) {
+		try {
+			terapiaRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
